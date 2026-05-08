c8290bfb49413a945c44a2eee1ad70cc2ba1ab6f
Michael-Nyawade <mikeokello17@gmail.com>
2026-05-08 22:06:52 +0300

    Customized admin interface for inventory management

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register models
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'sku',
+        'stock_quantity',
+        'buying_price',
+        'selling_price',
+        'profit_per_item',
+        'low_stock_status',
+    )
+
+    list_filter = ('category',)
+    search_fields = ('name', 'sku')
+
+    def low_stock_status(self, obj):
+        return obj.is_low_stock()
+
+    low_stock_status.boolean = True
+    low_stock_status.short_description = 'Low Stock'
