26e14a9ecc10076ad8e64c7297b54416d1cfa321
Michael-Nyawade <mikeokello17@gmail.com>
2026-08-07 07:29:55 +0300

    Add reusable shop access mixin

--- core/mixins.py
+++ core/mixins.py
@@ -1,2 +1,24 @@
-class ShopModelMixin:
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+
+from core.utils import get_user_shop
+
+
+class ShopRequiredMixin(LoginRequiredMixin):
+    """
+    Provides access to the current user's shop.
+
+    Any view using this mixin gets a validated shop attribute.
+    """
+
     shop = None
+
+    def dispatch(self, request, *args, **kwargs):
+        self.shop = get_user_shop(request.user)
+
+        if self.shop is None:
+            raise PermissionDenied(
+                "User is not assigned to a shop."
+            )
+
+        return super().dispatch(request, *args, **kwargs)
